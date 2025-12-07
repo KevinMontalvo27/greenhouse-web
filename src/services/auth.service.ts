32ad3a4c2755ba482d5d5678ba0ccf32eaf7acfb
KevinMontalvo27 <montalvokevin375@gmail.com>
2025-12-07 15:49:05 -0700

    servicio de auth

---
+++ src/services/auth.service.ts
@@ -0,0 +1,19 @@
+import apiClient from './api/apiClient';
+import ENDPOINTS from './api/endpoints';
+import type { UserLogin, LoginResponse } from '@/types';
+
+class AuthService {
+    async login(credentials: UserLogin): Promise<LoginResponse> {
+        const response = await apiClient.post<LoginResponse>(
+            ENDPOINTS.AUTH.LOGIN,
+            credentials
+        );
+        return response.data;
+    }
+}
+
+export default new AuthService();
+
+
+
+
